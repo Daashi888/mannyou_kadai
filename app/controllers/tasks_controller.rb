c231928cd14b1e1d0f474fff066e6c1756f042e8
Kenji <fcb141021@yahoo.co.jp>
2023-09-04 00:10:28 +0900

    タスクの一覧画面、詳細画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,26 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def create
+    @task = Task.new(task_params)
+    redirect_to new_task_path
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:blog).permit(:title, :content)
   end
 end
