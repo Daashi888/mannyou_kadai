fcd6aedc67049451a559769342e84521c0c86df9
Kenji <fcb141021@yahoo.co.jp>
2023-09-15 16:24:39 +0900

    ユーザが作成したタスクの一覧を、ユーザのマイページで見られる機能の追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,24 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+  end
 end
